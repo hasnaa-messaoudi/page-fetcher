3623c4b8a7fd02d7ef395bd11b485a5492a5bdb4
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-26 15:25:10 +0000

    add file size dynamisation

--- fetcher.js
+++ fetcher.js
@@ -4,9 +4,11 @@ const request = require('request');
 const fs = require('fs');
 request(url, (error, response, body) => {
   if (response && response.statusCode == 200) {
+    let stats = fs.statSync(localFile);
+    let fileSizeInB = stats["size"];
     fs.writeFile(localFile, body, function (err) {
       if (err) throw err;
-      console.log(`Downloaded and saved 3261 bytes to ${localFile}`);
+      console.log(`Downloaded and saved ${fileSizeInB} bytes to ${localFile}`);
     });
     
   } else {
